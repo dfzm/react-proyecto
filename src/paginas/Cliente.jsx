0340a179c3e2201c97ccefc36e9f6541b06e077b
Daniel Zapata <danielzapatam93@gmail.com>
2025-02-28 12:58:41 +0100

    Cuarto commit

--- src/paginas/Cliente.jsx
+++ src/paginas/Cliente.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const Cliente = () => {
   const { id } = useParams();
   return (
     <>
       <h1>Perfil del cliente: #{id}</h1>
+      <Link to="/"> Volver al Dashboard</Link>
     </>
   );
 };
--- src/paginas/Dashboard.jsx
+++ src/paginas/Dashboard.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <>
       <h1>Dashboard: ¡Bienvenido al CRM! </h1>
+      <p>Selecciona un cliente para ver su perfil:</p>
+
+      <ul>
+        <li>
+          <Link to="/cliente/1">Cliente 1</Link>
+        </li>
+        <li>
+          <Link to="/cliente/2">Cliente 2</Link>
+        </li>
+        <li>
+          <Link to="/cliente/3">Cliente 3</Link>
+        </li>
+      </ul>
     </>
   );
 };
